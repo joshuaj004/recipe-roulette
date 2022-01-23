2d6344a79e51f388c05c9a1e159d014cdde0a1c4
Josh <joshuaj004@gmail.com>
2022-01-23 17:17:34 -0500

    Added a random recipe endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from typing import Optional, Tuple, List
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import func, select
 from starlette.responses import RedirectResponse
 
 from . import schemas
@@ -13,7 +14,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency
+
 def get_db():
     try:
         db = SessionLocal()
@@ -21,13 +22,21 @@ def get_db():
     finally:
         db.close()
 
+
 @app.get("/")
 def read_root():
     return RedirectResponse(url="/docs/")
 
+
 @app.get("/recipes/", response_model=List[schemas.Recipe])
 def read_all_recipes(db: Session = Depends(get_db)):
     record = db.query(models.Recipe).all()
+    return record
+
+
+@app.get("/recipes/random", response_model=schemas.Recipe)
+def read_random_recipe(db: Session = Depends(get_db)):
+    record = db.query(models.Recipe).order_by(func.random()).first()
     return record
 
 
@@ -36,10 +45,11 @@ def read_recipe(db: Session = Depends(get_db), recipe_id: int = None):
     record = db.query(models.Recipe).get(recipe_id)
     return record
 
+
 @app.post("/recipes/")
-def create_recipe(recipe_name: str, db: Session = Depends(get_db)):
+def create_recipe(recipe_name: str, vegearian: bool, db: Session = Depends(get_db)):
     db_record = models.Recipe(
-        name = recipe_name
+        name=recipe_name
     )
     db.add(db_record)
     db.commit()
