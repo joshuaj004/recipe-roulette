3785a0d0038ffbf2dc16807cab36aa63a9e3d428
Josh <joshuaj004@gmail.com>
2022-01-29 14:58:41 -0500

    Add auto loading recipe table with sortable columns

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,23 @@
 RECIPE_BASE_URL = "http://127.0.0.1:8000";
 
-// $("#RecipeButton").click(function (event) {
-//     $.getJSON(RECIPE_BASE_URL + '/recipes/', {
-
-//     }).done(function (data) {
-//         console.log(data);
-//     });
-// });
-
-
 $("#RandomRecipeButton").click(function (event) {
     $.getJSON(RECIPE_BASE_URL + '/recipes/random/', {
 
     }).done(function (data) {
         $("#RecipeName").text(data.name);
-        console.log(data.name);
-        console.log(data);
     });
+});
+
+function populateTable() {
+    $.getJSON(RECIPE_BASE_URL + '/recipes/', {
+
+    }).done(function (data) {
+        $('table').bootstrapTable({
+            data: data
+        });
+    });
+}
+
+$(document).ready(function () {
+    populateTable();
 });
